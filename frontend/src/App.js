d962cab0e74c48e4b8f49ae09f5e979905c7f646
Kristin <kristin@iMac.local>
2023-01-06 15:34:53 +0100

    changed netlify.toml

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,10 +6,10 @@ import Summary from 'components/Summary';
 import Header from 'components/Header';
 import GuestBook from 'components/Guestbook/GuestBook';
 import Book from 'components/Booking/Book';
-// import Info from 'components/Info';
-// import Breakfast from 'components/Breakfast';
-// import Events from 'components/Events';
-// import AboutUs from 'components/AboutUs';
+import Info from 'components/Info';
+import Breakfast from 'components/Breakfast';
+import Events from 'components/Events';
+import AboutUs from 'components/AboutUs';
 
 export const App = () => {
   return (
@@ -19,13 +19,13 @@ export const App = () => {
           <Routes>
           <Route path='/' element={<Start />} />
           <Route path='/summary' element={<Summary />} />
-          {/* <Route path='/events' element={<Events />} />
-          <Route path='/aboutus' element={<AboutUs />} /> */}
+          <Route path='/events' element={<Events />} />
+          <Route path='/aboutus' element={<AboutUs />} />
           <Route path='/guestbook' element={<GuestBook />} />
           <Route path='/book' element={<Book />} />
-          {/* <Route path='/info' element={<Info />} />
+          <Route path='/info' element={<Info />} />
           <Route path='/aboutus' element={<AboutUs />} />
-          <Route path='/breakfast' element={<Breakfast />} /> */}
+          <Route path='/breakfast' element={<Breakfast />} />
           </Routes>
         </BrowserRouter>
         
